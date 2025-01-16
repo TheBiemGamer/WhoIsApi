a91f35bfe0440e58c4bdd2535a93f707e53fd54b
TheBiemGamer <47662596+TheBiemGamer@users.noreply.github.com>
2025-01-16 23:01:14 +0100

    Update app.py

--- whoisapi/app.py
+++ whoisapi/app.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify, render_template_string, send_from_directory
+from flask import Flask, jsonify, render_template_string, url_for
 import whois, re, os
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='favicon.ico'))
 
 @app.route("/")
 def index():
