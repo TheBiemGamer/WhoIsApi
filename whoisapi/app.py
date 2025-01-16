8fc3eef847697ccaff866e53d33b2f2dbf1a6b34
TheBiemGamer <47662596+TheBiemGamer@users.noreply.github.com>
2025-01-16 22:31:40 +0100

    Add favicon

--- whoisapi/app.py
+++ whoisapi/app.py
@@ -15,6 +15,7 @@ def index():
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>WHOIS Lookup</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
         <style>
             :root {
